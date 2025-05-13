c594b443a79c8b3ad6e668e223ced8c4ab7b5a31
ServetWb <swbb34@hotmail.com>
2025-05-13 17:06:08 -0500

    First jUnit

---
+++ src/test/java/Tests/Day06_JUnit_FrameWork/L01_Test_Annotation.java
@@ -0,0 +1,36 @@
+package Tests.Day06_JUnit_FrameWork;
+
+import org.junit.jupiter.api.Test;
+
+public class L01_Test_Annotation {
+
+    /*
+    Until now, we’ve said that a class must have a main method
+    in order to be runnable.
+
+    And indeed, without a main method,
+    the run button does not appear next to the methods.
+
+    The biggest advantage that comes with the JUnit framework
+    is the @Test annotation.
+
+    Thanks to the @Test annotation,
+    each method can be executed independently.
+ */
+
+    @Test
+    public void test04() {
+        System.out.println("Test4 executed");
+    }
+
+    @Test // @Disabled
+    public void test02() {
+        System.out.println("Test2 executed");
+    }
+
+    @Test
+    public void test03() {
+        System.out.println("Test3 executed");
+
+    
+}
---
+++ src/test/java/Tests/Day06_JUnit_FrameWork/L02_IfElse_Test.java
@@ -0,0 +1,29 @@
+package Tests.Day06_JUnit_FrameWork;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+
+public class L02_IfElse_Test {
+
+    public  void testOtotmasyonuTesti(){
+        WebDriver driver= new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+
+
+
+    }
+    public void wiseQuarterTest(){
+
+    }
+    public  void YoutubeTest() {
+
+
+    }
+    }
+
